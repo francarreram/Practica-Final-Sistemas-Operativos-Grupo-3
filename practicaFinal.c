f1c7d80a2f5199067f3a8e4c864767c29f42f6c5
InesCifuentes <72549553+InesCifuentes@users.noreply.github.com>
2023-01-07 18:00:02 +0100

    subo accionesTecnico

--- practicaFinal.c
+++ practicaFinal.c
@@ -13,8 +13,8 @@
 void nuevoClienteRed(int tipo);
 void *accionesCliente(void *ptr);
 void eliminarCliente(struct Cliente **clienteAEliminar);
-void accionesTecnico(void *ptr);
-void accionesEncargado(void *ptr);
+void *accionesTecnico(void *ptr);
+void *accionesEncargado(void *ptr);
 void accionesTecnicoDomiciliario(void *ptr);
 void terminar(int señal);
 void escribeEnLog(char *id, char *msg);
@@ -23,6 +23,7 @@ int calculaAleatorios(int min, int max);
 pthread_mutex_t mutexFichero;
 pthread_mutex_t mutexColaClientes;
 pthread_mutex_t mutexSolicitudesDom;
+pthread_mutex_t mutexSolicitudesTecnicos;
 
 pthread_cond_t condicionTecnicoDomiciliario; 
 pthread_cond_t condicionClienteDomiciliario;
@@ -112,27 +113,39 @@ int main(int argc, char const *argv[]){
         (listaClientes+i)->solicitud = 0;
     }
 
-    //Inicializar lista de tecnicos
+    //Inicializar lista de tecnicos (2 tecnico app, 2 tecnico red, 1 encargado y 1 tecnico domiciliario)
     listaTecnicos = (struct Tecnico*)malloc(sizeof(struct Tecnico)*6);
 
     for (int i = 0; i < 6; i++) {
         switch(1){
+
+            //Tecnicos app
             case 0:
+            (listaTecnicos+i)->tipo = 0;
+                (listaTecnicos+i)->solicitudes = 0;
+                (listaTecnicos+i)->atendiendo = 0;
+                break;
             case 1:
                 (listaTecnicos+i)->tipo = 0;
                 (listaTecnicos+i)->solicitudes = 0;
                 (listaTecnicos+i)->atendiendo = 0;
                 break;
+            
+            //Encargado
             case 4:
                 (listaTecnicos+i)->tipo = 2;
                 (listaTecnicos+i)->solicitudes = 0;
                 (listaTecnicos+i)->atendiendo = 0;
                 break;
+            
+            //Tecnico dommiciliario
             case 5:
                 (listaTecnicos+i)->tipo = 3;
                 (listaTecnicos+i)->solicitudes = 0;
                 (listaTecnicos+i)->atendiendo = 0;
                 break;
+
+            //Tecnicos red
             default:
                 (listaTecnicos+i)->tipo = 1;
                 (listaTecnicos+i)->solicitudes = 0;
@@ -152,12 +165,13 @@ int main(int argc, char const *argv[]){
     if (pthread_cond_init(&condicionClienteDomiciliario, NULL) != 0) exit(-1);
 
     //Crear los 6 hilos 
-    pthread_create(&listaTecnicos[0].hiloTecnico, NULL, accionesTecnico, NULL);
-    pthread_create(&listaTecnicos[1].hiloTecnico, NULL, accionesTecnico, NULL);
-    pthread_create(&listaTecnicos[2].hiloTecnico, NULL, accionesTecnico, NULL);
-    pthread_create(&listaTecnicos[3].hiloTecnico, NULL, accionesTecnico, NULL);
-    pthread_create(&listaTecnicos[4].hiloTecnico, NULL, accionesEncargado, NULL);
-    pthread_create(&listaTecnicos[5].hiloTecnico, NULL, accionesTecnicoDomiciliario, NULL);
+    int pos[6]={0,1,2,3,4,5}
+    pthread_create(&listaTecnicos[0].hiloTecnico, NULL, accionesTecnico, (void*)&pos[0]);
+    pthread_create(&listaTecnicos[1].hiloTecnico, NULL, accionesTecnico, (void*)&pos[1]);
+    pthread_create(&listaTecnicos[2].hiloTecnico, NULL, accionesTecnico, (void*)&pos[2]);
+    pthread_create(&listaTecnicos[3].hiloTecnico, NULL, accionesTecnico, (void*)&pos[3]);
+    pthread_create(&listaTecnicos[4].hiloTecnico, NULL, accionesEncargado, (void*)&pos[4]);
+    pthread_create(&listaTecnicos[5].hiloTecnico, NULL, accionesTecnicoDomiciliario, (void*)&pos[5]);
 
     //Esperar señales de forma infinita
     while(TRUE){
@@ -353,6 +367,150 @@ void *accionesCliente(void *ptr) {
     pthread_mutex_unlock(&mutexColaClientes);
 
 }
+
+void *accionesTecnico(void *ptr){
+
+    while(TRUE){  
+
+        //Guarda la posicion del tecnico en la listaTecnicos
+        int idTecnico = *(int *)ptr;
+
+        //1. El tecnico busca al cliente de su tipo con mas prioridad o que lleve mas tiempo esperando
+
+        //1.1 Tecnico app se encarga de atender a los clientes app
+        if( (idTecnico==0 || idTecnico==1) && listaTecnicos[idTecnico].atendiendo==0 ){
+            pthread_mutex_lock(&mutexSolicitudesTecnicos);
+            //Obtenemos posicion del cliente con mayor prioridad
+            int pos=-1;
+            int count=0;
+            for(int i=0; int<numSolicitudes; i++){
+
+                idTecnico = *(int *)ptr;
+                if(listaClientes[i].tipo==0 && listaClientes[i].atendido==0){
+                    ++count;
+                    if(listaClientes[i].prioridad>listaClientes[pos].prioridad){
+                        pos=i;
+                    }
+                        
+                }
+            }
+
+            //a. Si no hay clientes de su tipo espera 3s y vuelve a 1
+            if(count==0){
+                sleep(3);
+                pthread_mutex_unlock(&mutexSolicitudesTecnicos);
+            }else{
+
+                //2. cambiamos el flag 
+                listaTecnicos[idTecnico].atendiendo=1; 
+                listaTecnicos[idTecnico].solicitudes++;
+
+                //3. Calculamos el tipo de atencion
+                int num = calculaAleatorios(0,100);
+
+                //80% tiene todo en orden
+                if(num<80){
+                    sleep(calculaAleatorios(1,4));
+                    //Falta logs y enviar solicitud dom
+
+                //10% mal identificados
+                }else if(num<90){
+                    sleep(calculaAleatorios(2,6));
+                    //Falta logs y enviar solicitud dom
+
+                //10% se ha confunfido de compañia
+                }else{
+                    sleep(calculaAleatorios(1,2));
+                    //Falta logs y abandonar el sistema
+                }
+
+                //8. Cambiamos flag
+                listaClientes[pos].atendido=1; 
+
+                //9. Tecnico mira si tiene que descansar
+                if(listaTecnicos[idTecnico].solicitudes>=5){
+                    sleep(5);
+                }
+
+                //Desbloqueamos el mutex y volvemos a 1 
+                pthread_mutex_unlock(&mutexSolicitudesTecnicos);
+
+            }
+        }
+
+        //1.2 Tecnico red se encarga de atender a los clientes red
+        if((idTecnico==2 || idTecnico==3) && listaTecnicos[idTecnico].atendiendo==0 ){
+            pthread_mutex_lock(&mutexSolicitudesTecnicos);
+            //Obtenemos posicion del cliente con mayor prioridad
+            int pos=-1;
+            int count=0;
+            for(int i=0; int<numSolicitudes; i++){
+
+                idTecnico = *(int *)ptr;
+                if(listaClientes[i].tipo==1 && listaClientes[i].atendido==0){
+                    ++count;
+                    if(listaClientes[i].prioridad>listaClientes[pos].prioridad){
+                        pos=i;
+                    }
+                        
+                }
+            }
+
+            //a. Si no hay clientes de su tipo espera 3s y vuelve a 1
+            if(count==0){
+                sleep(3);
+                pthread_mutex_unlock(&mutexSolicitudesTecnicos);
+            }else{
+
+                //2. cambiamos el flag 
+                listaTecnicos[idTecnico].atendiendo=1; 
+                listaTecnicos[idTecnico].solicitudes++;
+
+                //Calculamos el tipo de atencion
+                int num = calculaAleatorios(0,100);
+
+                //80% tiene todo en orden
+                if(num<80){
+                    sleep(calculaAleatorios(1,4));
+                    //Falta logs y enviar solicitud dom
+
+                //10% mal identificados
+                }else if(num<90){
+                    sleep(calculaAleatorios(2,6));
+                    //Falta logs y enviar solicitud dom
+
+                //10% se ha confunfido de compañia
+                }else{
+                    sleep(calculaAleatorios(1,2));
+                    //Falta logs y abandonar el sistema
+                }
+
+                //8. Cambiamos flag
+                listaClientes[pos].atendido=1; 
+
+                //9. Tecnico mira si tiene que descansar
+                if(listaTecnicos[idTecnico].solicitudes>=5){
+                    sleep(5);
+                }
+
+                //Desbloqueamos el mutex y volvemos a 1 
+                pthread_mutex_unlock(&mutexSolicitudesTecnicos);
+
+            }
+
+        }
+    }
+
+
+}
+
+
+
+void *accionesEncargado(void *ptr){
+
+}
+
+
 
 void eliminarCliente(struct Cliente **clienteAEliminar){
     pthread_mutex_lock(&mutexColaClientes);
